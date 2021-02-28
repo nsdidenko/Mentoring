c8500902acef25d5119de32c203deda64af4fb72
Nikita <nsdidenko.developer@gmail.com>
2021-02-28 15:39:45 +0300

    Improve naming of tests to be more descriptive

--- MentoringTests/FizzBuzzTests.swift
+++ MentoringTests/FizzBuzzTests.swift
@@ -9,19 +9,19 @@ class FizzBuzzTests: XCTestCase {
         XCTAssertEqual(fizzBuzzed, ["1", "2", "7", "8"])
     }
     
-    func test_fizzBuzz_returnsFizzForAllNumbersMultipliesOfThree() {
+    func test_fizzBuzz_replacesAllNumbersMultipliesOfThreeWithFizz() {
         let fizzBuzzed = fizzBuzz([1, 3, 4, 9])
         
         XCTAssertEqual(fizzBuzzed, ["1", "Fizz", "4", "Fizz"])
     }
     
-    func test_fizzBuzz_returnsBuzzForAllNumbersMultipliesOfFive() {
+    func test_fizzBuzz_replacesAllNumbersMultipliesOfFiveWithBuzz() {
         let fizzBuzzed = fizzBuzz([2, 5, 7, 10])
         
         XCTAssertEqual(fizzBuzzed, ["2", "Buzz", "7", "Buzz"])
     }
     
-    func test_fizzBuzz_returnsFizzBuzzForAllNumbersMultipliesOfFifteen() {
+    func test_fizzBuzz_replacesAllNumbersMultipliesOfFifteenWithFizzBuzz() {
         let fizzBuzzed = fizzBuzz([1, 15, 19, 4])
         
         XCTAssertEqual(fizzBuzzed, ["1", "FizzBuzz", "19", "4"])
